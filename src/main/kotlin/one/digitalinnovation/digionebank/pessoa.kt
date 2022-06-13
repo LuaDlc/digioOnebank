b30e481f9560229c288dcd4a82e467e3f30cd924
LuaDlc <luadiaslc@gmail.com>
2022-06-13 16:38:28 -0300

    criei o projeto

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/pessoa.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digionebank
+
+class pessoa {
+    var nome: String = "Luana"
+    var cpf: String = "151.258.221.55"
+}
+fun main() {
+    val Luana = pessoa()
+    println(Luana.nome)
+    println(Luana.cpf)
+}
