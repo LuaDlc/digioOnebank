78e26cf945908c48406720594bfcf9739ac451b3
LuaDlc <luadiaslc@gmail.com>
2022-06-14 08:45:13 -0300

    private set adicionado para o cpf

--- src/main/kotlin/one/digitalinnovation/digionebank/pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/pessoa.kt
@@ -2,10 +2,16 @@ package one.digitalinnovation.digionebank
 
 class pessoa {
     var nome: String = "Luana"
+
     var cpf: String = "151.258.221.55"
-}
+    private set
+
+
+    }
+
 fun main() {
     val Luana = pessoa()
     println(Luana.nome)
     println(Luana.cpf)
+
 }
