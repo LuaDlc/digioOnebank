83cf2374613502252af8b629d9d424bad943bf84
LuaDlc <luadiaslc@gmail.com>
2022-06-20 10:29:09 -0300

    update to make person and employee abstract. update bank class teste

--- src/main/kotlin/one/digitalinnovation/digionebank/pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/pessoa.kt
@@ -1,11 +1,14 @@
 package one.digitalinnovation.digionebank
 
-class pessoa {
+ class Pessoa {
     var nome: String = "Luana"
 
-    var cpf: String = "151.258.221.55"
+    var cpf: String = "151.258.221-55"
     private set
-
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+        )
     constructor()
 
     fun pessoaInfo() = "$nome e $cpf"
@@ -14,7 +17,7 @@ class pessoa {
     }
 
 fun main() {
-    val Luana = pessoa()
-    println(Luana.pessoaInfo())
-
+    val Luana = Pessoa()
+    println(Luana.pessoa.Info())
 }
+
